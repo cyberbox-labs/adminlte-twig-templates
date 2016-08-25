a40ac039b65684ab76488ec3875cff9b57520884
don <don@cyberbox.org>
2016-08-25 11:10:09 +0800

    删除没用的代码，以及增加 buildForm 方法

--- src/Form.php
+++ src/Form.php
@@ -4,22 +4,16 @@ namespace AdminLTE_Twig_Tpl;
 
 class Form extends \ArrayObject
 {
-    private $twig;
-    private $tplNamespace;
     private $fields = [];
     private $allowFieldTypes = ['text', 'password'];
 
-    public function __construct(\Twig_Environment $twig)
+    public function __construct()
     {
-        $this->twig = $twig;
+        $this->buildForm();
+    }
 
-        $globals = $twig->getGlobals();
-        if (!isset($globals['tpl_namespace']))
-        {
-            throw new \RuntimeException('Twig global variable "tpl_namespace" is not set');
-        }
-
-        $this->tplNamespace = $globals['tpl_namespace'];
+    public function buildForm()
+    {
     }
 
     public function add($name, $type, array $options = [])
@@ -66,34 +60,8 @@ class Form extends \ArrayObject
         return true;
     }
 
-    // public function row($name, array $variables = [])
-    // {
-    //     echo $this->renderBlock('block/form_row.html.twig', $variables);
-    // }
-
     private function isAllowFieldType($type)
     {
         return in_array($type, $this->allowFieldTypes);
-    }
-
-    // private function renderBlock($name, array $data = [])
-    // {
-    //     $name = sprintf('@%s/%s', $this->tplNamespace, $name);
-    //     $template = $this->twig->loadTemplate($name);
-
-    //     return $template->render(['hello' => 'world']);
-    // }
-
-    // public function __call($name)
-    // {
-    //     if (isset($this->fields[$name]))
-    //     {
-    //         return $this->fields[$name];
-    //     }
-    // }
-
-    public function Repository()
-    {
-        return 1;
     }
 }
